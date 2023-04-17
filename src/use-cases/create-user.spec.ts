4a5f0d92eaacfb2fd59f64b7a6229f55f9fcc4cc
Ericlys Moreira <ericlysm@gmail.com>
2023-04-16 22:45:45 -0300

    chore: create user unit test

---
+++ src/use-cases/create-user.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { CreateUserUseCase } from './create-user'
+import { UsersRepository } from '@/repositories/users-repository'
+import { compare } from 'bcryptjs'
+import { UserAlreadyExistsError } from './erros/user-already-exists-error'
+
+let usersRepository: UsersRepository
+let sut: CreateUserUseCase
+
+describe('Create User Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new CreateUserUseCase(usersRepository)
+  })
+
+  it('should be able to create a new user', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'john.doe@exemple.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'john.doe@exemple.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to create a new user with same email twice', async () => {
+    const email = 'john.doe@example.com'
+
+    await sut.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
--- src/use-cases/create-user.ts
+++ src/use-cases/create-user.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from '@/repositories/users-repository'
 import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './erros/user-already-exists-error'
 
 interface CreateUserUseCaseRequest {
   name: string
@@ -12,7 +13,7 @@ interface CreateUserUseCaseResponse {
   user: User
 }
 
-export class CreateUser {
+export class CreateUserUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
   async execute({
@@ -20,6 +21,12 @@ export class CreateUser {
     email,
     password,
   }: CreateUserUseCaseRequest): Promise<CreateUserUseCaseResponse> {
+    const userEmailAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userEmailAlreadyExists) {
+      throw new UserAlreadyExistsError()
+    }
+
     const password_hash = await hash(password, 6)
 
     const user = await this.usersRepository.create({
