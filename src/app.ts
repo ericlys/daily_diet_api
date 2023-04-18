d694ac0c3ce3c627f078b72677f3ade8b01fd800
Ericlys Moreira <ericlysm@gmail.com>
2023-04-18 13:06:52 -0300

    chore: Authentication controller and fastify token

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { env } from './env'
+import { usersRoutes } from './http/controllers/users/routes'
+import { ZodError } from 'zod'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(usersRoutes)
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  } else {
+    // Todo: here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+
+import { authenticate } from './authenticate'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/sessions', authenticate)
+}
