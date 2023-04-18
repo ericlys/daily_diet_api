cf4b92423b8fec4f211b61fbbee280ff1e10d83c
Ericlys Moreira <ericlysm@gmail.com>
2023-04-18 14:01:21 -0300

    chore: Delete meals controller

---
+++ src/http/controllers/meals/remove.ts
@@ -0,0 +1,28 @@
+import { ResourceNotFoundError } from '@/use-cases/erros/resource-not-found-error'
+import { makeDeleteMealUseCase } from '@/use-cases/factories/make-delete-meal-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function remove(request: FastifyRequest, reply: FastifyReply) {
+  const deleteMealBodyParams = z.object({
+    id: z.string().uuid(),
+  })
+
+  const { id } = deleteMealBodyParams.parse(request.params)
+
+  const mealUseCase = makeDeleteMealUseCase()
+
+  try {
+    await mealUseCase.execute({
+      id,
+    })
+  } catch (err) {
+    if (err instanceof ResourceNotFoundError) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+
+  return reply.status(204).send()
+}
--- src/http/controllers/meals/routes.ts
+++ src/http/controllers/meals/routes.ts
@@ -1,9 +1,11 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { remove } from './remove'
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/meals', create)
+  app.delete('/meals/:id', remove)
 }
